aeed27032cfecb43f0e14cc2f29ee96dfe3228f1
Molly <yun.liu@smartmore.com>
2023-08-22 17:30:37 +0800

    更改run

--- run.py
+++ run.py
@@ -5,9 +5,6 @@ open_Software.open_sofeware(r".\VDL.exe")
 open_Software.connect_sofeware("Windows:///?title_re=MainWindow.*")
 open_Software.click_maximize() 
 
-#run_command = ['C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest_0804\\testcase\\test_01_openproj.py',
-#               'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest_0804\\testcase\\test_02_inferpage.py']
-#'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest_0804\\testcase\\testcase_openproj.py'
 
 # run_command = [ 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest_0804\\testcase\\test01_management.py',
 #                 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest_0804\\testcase\\test02_data.py',
