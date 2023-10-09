25db304ef042ded068ad7f70c77de9f68a71849b
liuyun <yun.liu@smartmore.com>
2023-10-09 17:25:52 +0800

    1

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import pytest
+import os
 from pages.open_sofrware import open_Software
 
 open_Software.open_sofeware(r".\VDL.exe")
 open_Software.connect_sofeware("Windows:///?title_re=MainWindow.*")
 open_Software.click_maximize() 
 
-run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test01_management.py','--alluredir=reports/']
+run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test01_management.py', '--alluredir=./reports']
 
 
 # run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase01\\test_01_openproj.py',
@@ -19,3 +20,5 @@ run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test01_management.
 # run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test00_smoke.py']
 
 pytest.main(run_command) 
+
+# os.system('allure serve reports')
