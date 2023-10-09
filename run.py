54d40c6c79cde82dbef82a5b0c0fa1a825a2372d
liuyun <yun.liu@smartmore.com>
2023-10-09 16:03:12 +0800

    测试

--- run.py
+++ run.py
@@ -5,17 +5,17 @@ open_Software.open_sofeware(r".\VDL.exe")
 open_Software.connect_sofeware("Windows:///?title_re=MainWindow.*")
 open_Software.click_maximize() 
 
-#run_command = ['C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest_0804\\testcase\\test_01_openproj.py',
-#               'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest_0804\\testcase\\test_02_inferpage.py']
+run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase01\\test_01_openproj.py']
 
 
-# run_command = [ 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest\\testcase\\test01_management.py',
-#                 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest\\testcase\\test02_data.py',
-#                 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest\\testcase\\test03_label.py',
-#                 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest\\testcase\\test04_training.py',
-#                 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest\\testcase\\test05_evaluate.py',
-#                 'C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest\\testcase\\test06_infer.py']
+# run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase01\\test_01_openproj.py',
+#                 'D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test01_management.py',
+#                 'D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test02_data.py',
+#                 'D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test03_label.py',
+#                 'D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test04_training.py',
+#                 'D:\\ly\VDL_autotest\\VDL_autotest\\testcase\\test05_evaluate.py',
+#                 'D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test06_infer.py']
 
-run_command = ['C:\\Users\\yunli\\Desktop\\ly_autotest\\VDL_autotest\\testcase\\test_02_inferpage.py']
+# run_command = ['D:\\ly\\VDL_autotest\\VDL_autotest\\testcase\\test00_smoke.py']
 
 pytest.main(run_command) 
