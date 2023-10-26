aa29db0a64ac74c1bc61f4bed27ec54f75d743d7
liuyun <yun.liu@smartmore.com>
2023-10-26 09:54:58 +0800

    修改open_software文件

--- pages/open_sofrware.py
+++ pages/open_sofrware.py
@@ -8,21 +8,21 @@ class open_Software():
     os.chdir(r"D:\ViMo-Deeplearning")
 
     
-    def open_sofeware(self, app_path):
+    def open_sofeware(app_path):
         '''打开软件'''
         app = Application().start(app_path)
         # start_app(app_path)
         airtest_method.operate_sleep(10.0)
 
     
-    def connect_sofeware(self, url):
+    def connect_sofeware(url):
         '''连接当前设备'''
         airtest_method.connect_app(url)
         airtest_method.operate_sleep(5.0)
         #将当前窗口截图
         airtest_method.screenshot("当前窗口截图.png")
    
-    def click_maximize(self):
+    def click_maximize():
         '''点击窗口最大化'''
         airtest_method.assert_method(control.max_screen,"最大化按钮打开失败")
         airtest_method.touch_button(control.max_screen)        
