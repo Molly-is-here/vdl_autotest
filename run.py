a6670fd695bdbfb7fea596ae5a6e85540e827776
yun.liu <yun.liu@smartmore.com>
2024-11-13 14:16:36 +0800

    编辑模型卡片功能修改

--- run.py
+++ run.py
@@ -20,9 +20,9 @@ run_command = [
                 f'{addpath}/testcase/test06_infer.py',
                 f'{addpath}/testcase/test07_menu.py',                    
                 f'{addpath}/testcase/test10_autolabel.py',
-                # f'{addpath}/testcase/test00_smoke.py',
-                # f'{addpath}/testcase/test08_pipelines.py',
-                # f'{addpath}/testcase/test11_dark.py',
+                f'{addpath}/testcase/test00_smoke.py',
+                f'{addpath}/testcase/test08_pipelines.py',
+                f'{addpath}/testcase/test11_dark.py',
                 # f'{addpath}/testcase/test09_compatible.py', 
                 f'--alluredir={addpath}/report', '--clean-alluredir']
 
